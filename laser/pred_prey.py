952e2edfe699aac8eb4b63df7eea404aa2cc8ee0
kommander-keene <kboondich@gmail.com>
2024-10-03 22:14:57 -0400

    spiking yamada laser

--- laser/pred_prey.py
+++ laser/pred_prey.py
@@ -6,36 +6,37 @@ from diffrax import diffeqsolve, ControlTerm, MultiTerm, ODETerm, SaveAt, Virtua
 
 import jax
 
-class Yamada():
+class PredPreyEQ():
     """
-    Generic Yamada model laser
+    Predator-Prey testing class
     """
-    def __init__(self, params: tuple, initial_state=(0, 0, 0), solver=diffrax.Tsit5()):
+    def __init__(self, params: tuple, initial_state=(0, 0), solver=diffrax.Tsit5()):
         self.params = params
         self.initial_state = initial_state
         self.solver = solver
-        self.name = "yamada"  
-    def simple_yamada(self, inputs, sim_step):
+        self.name = "predprey"  
+    def simple_yamada(self):
+        def vector_field(t, y, args):
+            prey, predator = y
+            α, β, γ, δ = args
+            d_prey = α * prey - β * prey * predator
+            d_predator = -γ * predator + δ * prey * predator
+            d_y = d_prey, d_predator
+            return d_y
         def f(t, y, args):
-            nonlocal inputs, sim_step
             G, Q, I = y
-            A, B, a, y_G, y_Q, y_I = args
+            A, B, a, y_G, y_Q, y_I, _ = args
             d_G = y_G * (A - G - G * I)
             d_Q = y_Q * (B - Q - a * Q * I)
             d_I = y_I * (G - Q - 1) * I
             d_y = d_G, d_Q, d_I
             return d_y
-        return f
-    def sim(self, inputs, start_time, end_time, step):
-        if (not inputs):
-            inputs = jnp.zeros(shape=int((end_time-start_time)/step)) # inputs of zeros
-        terms = ODETerm(self.simple_yamada(inputs, step))
-
+        return vector_field
+    def sim(self, start_time, end_time, step):
+        terms = ODETerm(self.simple_yamada())
         assert(isinstance(terms, ODETerm))
         assert(isinstance(self.initial_state, tuple))
-
         saveat = SaveAt(ts=jnp.linspace(start_time, end_time, 1000))
-        
         sol = diffeqsolve(terms,
                           self.solver,
                           start_time,
@@ -47,15 +48,14 @@ class Yamada():
         self.sol = sol
     def plot(self):
         sol = self.sol
-        plt.plot(sol.ts, sol.ys[0], label="Gain")
-        plt.plot(sol.ts, sol.ys[1], label="Absorption")
-        plt.plot(sol.ts, sol.ys[2], label="Laser Intensity")
+        plt.plot(sol.ts, sol.ys[0], label="Prey")
+        plt.plot(sol.ts, sol.ys[1], label="Predator")
         plt.legend()
 
         plt.savefig(f"{self.name}_save.png")
         plt.show()
 
 if __name__ == "__main__":
-    yamada = Yamada(params=(0.1, 0.02, 0.4, 0.02, 0.5, 0.5), initial_state=(10., 10., 10.))
-    yamada.sim(None, 0, 140, .1)
+    yamada = PredPreyEQ(params=(0.1, 0.02, 0.4, 0.02), initial_state=(10., 10.))
+    yamada.sim(0, 140, 0.1)
     yamada.plot()
