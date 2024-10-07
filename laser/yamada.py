023e7d42af5ddbc8fd97fd357cce63842cc1f3f9
kommander-keene <kboondich@gmail.com>
2024-10-06 23:18:12 -0400

    forcing term

--- laser/yamada.py
+++ laser/yamada.py
@@ -10,17 +10,28 @@ class Yamada():
     """
     Generic Yamada model laser
     """
-    def __init__(self, params: tuple, initial_state=(0, 0, 0), solver=diffrax.Tsit5()):
+    def __init__(self, params: tuple, initial_state=(0, 0, 0), solver=diffrax.Tsit5(), radius=0.001):
         self.params = params
         self.initial_state = initial_state
         self.solver = solver
-        self.name = "yamada"  
+        self.name = "yamada"
+        self.spike_radius = radius
     def simple_yamada(self, inputs, sim_step):
+        def delta(t):        
+            return jnp.exp(-jnp.pow(t, 2)/(2*self.spike_radius))
+        def force(t, args):
+            A, B, a, y_G, y_Q, y_I, eps, spikes = args
+            spike = 0.0
+            if (spikes and len(spikes) == 0):
+                return 0.0
+            for A, i in spikes:
+                spike += A*delta(t - i)
+            return spike
         def f(t, y, args):
             nonlocal inputs, sim_step
             G, Q, I = y
-            A, B, a, y_G, y_Q, y_I = args
-            d_G = y_G * (A - G - G * I)
+            A, B, a, y_G, y_Q, y_I, eps, spikes = args
+            d_G = y_G * (A - G - G * I) + force(t, args)
             d_Q = y_Q * (B - Q - a * Q * I)
             d_I = y_I * (G - Q - 1) * I
             d_y = d_G, d_Q, d_I
@@ -56,6 +67,7 @@ class Yamada():
         plt.show()
 
 if __name__ == "__main__":
-    yamada = Yamada(params=(0.1, 0.02, 0.4, 0.02, 0.5, 0.5), initial_state=(10., 10., 10.))
+    spikes = ((10, 20), (-10, 40))
+    yamada = Yamada(params=(0.1, 0.02, 0.4, 0.02, 0.5, 0.5, 0, spikes), initial_state=(0., 0., 0.))
     yamada.sim(None, 0, 140, .1)
     yamada.plot()
